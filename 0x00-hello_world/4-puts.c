6eab99dd29d82b02761da2becea7024f4e1d81c7
Oluwatobi-M <michaelblake1689@gmail.com>
2022-03-10 15:49:12 -0800

    hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - display a message
+ * Return: display the message
+ */
+int main(void)
+
+{
+
+	char comment[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(comment);
+
+	return (0);
+}
